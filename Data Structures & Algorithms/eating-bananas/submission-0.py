ef4027ff8d5eb5484c754df9cf7ae78f2f8213ad
William Duong <william.duong2023+github@gmail.com>
2026-06-09 07:28:38 +1000

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,35 @@
+# We want max value that is smaller than h.
+#
+# value and mid are inversely proportional
+
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        if h < len(piles):
+            return -1
+
+        low = 1
+        high = max(piles)
+
+        best = -1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            value = sum(map(lambda x: math.ceil(x / mid), piles))
+
+            print(f"low={low}, high={high}, mid={mid}, value={value}")
+
+            if value < h:
+                high = mid - 1
+
+                if value > sum(map(lambda x: math.ceil(x / best), piles)):
+                    best = mid
+
+            elif value > h:
+                low = mid + 1
+
+            else:
+                return mid
+
+        return best
