b8b6b2890c6eb7aeffb21524e2e0f9146d97e956
William Duong <william.duong2023+github@gmail.com>
2026-06-09 08:22:44 +1000

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    # We want the max `value` that is smaller than `h`.
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        if h < len(piles):
+            return int(math.inf)
+
+        low = 1
+        high = max(piles)
+
+        best = max(piles)
+
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            # Note: `value` and `mid` are inversely proportional.
+            value = sum(map(lambda x: math.ceil(x / mid), piles))
+
+            if value > h:
+                low = mid + 1
+                continue
+
+            # If `value` <= `h`.
+            high = mid - 1
+
+            if mid < best and value >= sum(map(lambda x: math.ceil(x / best), piles)):
+                best = mid
+
+        return best
