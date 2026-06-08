ff173ea5507b42c7932d5fd971c5c14a0d5ba297
William Duong <william.duong2023+github@gmail.com>
2026-06-09 08:40:56 +1000

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.py
@@ -0,0 +1,29 @@
+class Solution:
+    # We want the minimum eating speed `mid` such that the required hours `value` is <= `h`.
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        if h < len(piles):
+            return sys.maxsize
+
+        low = 1
+        high = max(piles)
+
+        best = max(piles)
+
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            # Note: `value` and `mid` are inversely proportional.
+            value = sum(math.ceil(p / mid) for p in piles)
+
+            if value > h:
+                low = mid + 1
+                continue
+
+            # If `value` <= `h`.
+            high = mid - 1
+
+            # Since `mid` < `best`, `value` is guaranteed to be >= the old best's `value`
+            if mid < best:
+                best = mid
+
+        return best
