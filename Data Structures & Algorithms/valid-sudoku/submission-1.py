ba340b63e7f3fd7a88b2d33dda6ec95ee164d56d
William Duong <william.duong2023+github@gmail.com>
2026-07-14 00:10:17 +1000

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        display(board)
+
+        row = set()
+        cols = defaultdict(set)
+        boxes = defaultdict(set)
+
+        for line, values in enumerate(board, start=1):
+            for col, value in enumerate(values, start=1):
+                if value == ".":
+                    continue
+
+                if value in row:
+                    return False
+                row.add(value)
+
+                if value in cols[col]:
+                    return False
+                cols[col].add(value)
+
+                box = math.ceil(col / 3)
+                if value in boxes[box]:
+                    return False
+                boxes[box].add(value)
+
+            row = set()
+            boxes.clear() if line % 3 == 0 else None
+
+        return True
+
+
+def display(board: List[List[str]]) -> None:
+    for line in board:
+        print(line)
